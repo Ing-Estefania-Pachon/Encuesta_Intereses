180586fffbf382942aa74ad6129a98fe5d3a37b3
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-22 20:24:43 -0500

    Ya es funcional

---
+++ script.js
@@ -0,0 +1,19 @@
+let votos = {
+    JavaScript : 0,
+    DiseñoWeb : 0,
+    BaseDatos : 0,
+}
+
+let totalVotos = 0
+
+function votar(click){
+    votos[click]++;
+    totalVotos ++;
+
+    alert("¡Gracias por tu voto!");
+
+    document.getElementById("votos-js").textContent = votos.JavaScript;
+    document.getElementById("votos-dw").textContent = votos.DiseñoWeb;
+    document.getElementById("votos-bd").textContent = votos.BaseDatos;
+  
+}
